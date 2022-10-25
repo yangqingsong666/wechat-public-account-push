a7dedded049189ea6cd47a2993489fafe43f2658
yangqingsong <115615672+yangqingsong666@users.noreply.github.com>
2022-10-25 10:53:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyJB76PYpGZjELCRuHq6RGiydvoc',
+      id: 'oyJB76AZa5KrbnNlUmAksC8zAGPs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'yCP4OqBwTjYq6Nuc7zTQfvLUEwdaEfZtUa-ULkZJLXc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
